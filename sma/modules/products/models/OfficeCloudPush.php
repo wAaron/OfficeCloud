a2e39dcc4a27b10b1bbf5fe2c6841d4f0af86aea
mwtee <mingwei.tee@gmail.com>
2014-12-15 10:04:38 +1100

    Before changes in file location for pusher

--- sma/modules/products/models/OfficeCloudPush.php
+++ sma/modules/products/models/OfficeCloudPush.php
@@ -1,12 +1,33 @@
 <?php
 include ('../sma/config/database.php');
+
+$vtigerConnection;
 function vtigerDbConnect()
 {
-	$vtigerConnection = mysql_connect ( vitger_hostname, vtiger_username, vtiger_password );
-	// $dbh2 = mysql_connect($hostname, $username, $password, true);
+	GLOBAL $vtigerConnection;
+	// Create connection
+	$vtigerConnection = new mysqli ( vtiger_hostname, vtiger_username, vtiger_password, vtiger_database );
+
+	// Check connection
+	if ($vtigerConnection->connect_error)
+	{
+		die ( "Connection failed: " . $vtigerConnection->connect_error );
+	}	
+
+// 	echo "Record updated successfully";
+// 	if (mysqli_query($vtigerConnection, $sql)) {
+// 		echo "Record updated successfully";
+// 	} else {
+// 		echo "Error updating record: " . mysqli_error($vtigerConnection);
+// 	}
+
+// 	$sql = "UPDATE vtiger_products SET productno='PRO11' WHERE productid=6";
+// 	mysqli_query($vtigerConnection, $sql);
+// // 	mysqli_close($vtigerConnection);
 	
-	mysql_select_db ( vtiger_database, $vtigerConnection );
-	// mysql_select_db('database2', $dbh2);
+	
+
+	// echo "Connected successfully";
 }
 function cloudPusher($productID, $data)
 {
@@ -14,6 +35,24 @@ function cloudPusher($productID, $data)
 	echo vtiger_username;
 	echo "code is ", $data ['code'];
 	echo "productID is ", $productID;
+	
+	GLOBAL $vtigerConnection;
+	$sql = "UPDATE vtiger_products SET productname='Chrome100' WHERE productid=6";
+	if(mysqli_query($vtigerConnection, $sql))
+	{
+		echo "Record updated successfully";
+	}
+ 	mysqli_close($vtigerConnection);
+	
+// 	if ($result = $GLOBALS['vtigerConnection']->query ( "SELECT productname FROM vtiger_products" ))
+// 	{
+// 		printf ( "Select returned %d rows.\n", $result->num_rows );
+		
+// 		/* free result set */
+// 		$result->close ();
+// 	}
+	
+
 	return true;
 }
 
